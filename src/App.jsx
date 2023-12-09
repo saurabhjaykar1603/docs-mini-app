72425cad4bcc22882a14b58c18b0742ad5469b3c
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-09 23:01:05 +0530

    style: Add styling to the main container in DocsMini component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1>home</h1>
+    <div className=" relative w-full h-screen bg-zinc-800">
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  text-[9.5vw] leading-none tracking-tighter font-semibold text-zinc-700 ">
+        DOCS MINI
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
