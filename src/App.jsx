3dc267d24bcb24b792a7b6c99747fd42865154ee
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-09 23:31:04 +0530

    feat: Create Foreground component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Background from "./components/Background/Background";
+import Foreground from "./components/Foreground/Foreground";
 
 function App() {
   return (
     <div className=" relative w-full h-screen bg-zinc-800">
       <Background />
+      <Foreground />
     </div>
   );
 }
---
+++ src/components/Foreground/Foreground.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Foreground() {
+  return (
+    <div>
+      <div className=" fixed z-[3] top-0 left-0 w-full h-full "></div>
+    </div>
+  );
+}
+
+export default Foreground;
