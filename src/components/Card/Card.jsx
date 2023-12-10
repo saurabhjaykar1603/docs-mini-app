d1867894b60776e8fbbf4418667571f1d7a0c441
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-10 12:19:02 +0530

    feat: Create Card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaRegFileAlt } from "react-icons/fa";
+import { IoDownload } from "react-icons/io5";
+
+function Card() {
+  return (
+    <div>
+      <div className=" relative w-60 h-72 rounded-[40px] bg-zinc-600 text-white px-8 py-10 overflow-hidden ">
+        <FaRegFileAlt />
+        <p className="text-sm mt-5 font-semibold leading-tight">
+          Lorem ipsum dolor sit amet consectetur adipisicing.
+        </p>
+        <footer className="footer absolute bottom-0  w-full   left-0 ">
+          <div className="flex items-center justify-between mb-3 py-4 px-8">
+            <h5>.4mb</h5>
+            <span className="w-6 h-6 bg-zinc-400 rounded-full flex items-center justify-center p-1">
+              <IoDownload size="8em" color="#000" />
+            </span>
+          </div>
+          <div className="tag w-full py-4  bg-green-600 flex items-center justify-center">
+            <h3 className="text-md font-semibold">Download Now</h3>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/Foreground/Foreground.jsx
+++ src/components/Foreground/Foreground.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Card from "../Card/Card";
 
 function Foreground() {
   return (
     <div>
-      <div className=" fixed z-[3] top-0 left-0 w-full h-full "></div>
+      <div className=" fixed z-[3] top-0 left-0 w-full h-full ">
+        <Card />
+      </div>
     </div>
   );
 }
