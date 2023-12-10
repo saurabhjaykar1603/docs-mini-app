bb1f7558f404e4de886bf395da996715c6007208
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-10 13:46:39 +0530

    feat: Enhance Card component for dynamic file representation

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { IoDownload } from "react-icons/io5";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
-function Card() {
+function Card({ data }) {
   return (
     <div>
-      <div className=" relative w-60 h-72 rounded-[40px] bg-zinc-600 text-white px-8 py-10 overflow-hidden ">
+      <div className=" flex shrink-0 relative w-60 h-72 rounded-[40px] bg-zinc-600 text-white px-8 py-10 overflow-hidden ">
         <FaRegFileAlt />
-        <p className="text-sm mt-5 font-semibold leading-tight">
-          Lorem ipsum dolor sit amet consectetur adipisicing.
+        <p className="text-sm mt-8 font-semibold leading-tight">
+          {data.description}
         </p>
         <footer className="footer absolute bottom-0  w-full   left-0 ">
           <div className="flex items-center justify-between mb-3 py-4 px-8">
-            <h5>.4mb</h5>
+            <h5>{data.filesize}</h5>
             <span className="w-6 h-6 bg-zinc-400 rounded-full flex items-center justify-center p-1">
-              <IoDownload size="8em" color="#000" />
+              {data.close ? (
+                <IoMdCloseCircleOutline />
+              ) : (
+                <IoDownload size="8em" color="#000" />
+              )}
             </span>
           </div>
-          <div className="tag w-full py-4  bg-green-600 flex items-center justify-center">
-            <h3 className="text-md font-semibold">Download Now</h3>
-          </div>
+          {data.tag.isOpen && (
+            <div
+              className={`tag w-full py-4  ${
+                data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"
+              } flex items-center justify-center`}
+            >
+              <h3 className="text-md font-semibold">{data.tag.tagTitle}</h3>
+            </div>
+          )}
         </footer>
       </div>
     </div>
--- src/components/Foreground/Foreground.jsx
+++ src/components/Foreground/Foreground.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Card from "../Card/Card";
+import data from "../../utils/data";
 
 function Foreground() {
+  
   return (
     <div>
-      <div className=" fixed z-[3] top-0 left-0 w-full h-full ">
-        <Card />
+      <div className=" fixed z-[3] top-0 left-0 w-full h-full flex gap-10 flex-wrap p-6">
+        {data.map((item, i) => (
+          <Card data={item} />
+        ))}
       </div>
     </div>
   );
