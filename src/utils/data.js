e31667f928187f600aa6eef4a665c3aa5aa2ae9f
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-10 13:45:35 +0530

    feat (config UI): Add data  array for file descriptions and actions

---
+++ src/utils/data.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    description: "my resume has been put here where i can download it ",
+    filesize: ".4mb",
+    close: false,
+    tag: { isOpen: true, tagTitle: "Download now", tagColor: "green" },
+  },
+  {
+    description: "hi am here for Uploading Your files",
+    filesize: ".5mb",
+    close: true,
+    tag: { isOpen: true, tagTitle: "Upload now", tagColor: "blue" },
+  },
+  {
+    description: "lorem Ips incorrectly formatted incorrectly  in HTML",
+    filesize: ".4mb",
+    close: true,
+    tag: { isOpen: false, tagTitle: "upload", tagColor: "green" },
+  },
+];
+export default data;
